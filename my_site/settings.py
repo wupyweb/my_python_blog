44c43f0bbc6adba701577de0d6879c314a78a2fc
wufujie <wufujie@qq.com>
2019-07-05 10:49:14 +0800

    使用MySQL

--- my_site/settings.py
+++ my_site/settings.py
@@ -78,10 +78,18 @@ WSGI_APPLICATION = 'my_site.wsgi.application'
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+            'ENGINE': 'django.db.backends.mysql',
+            'HOST': '127.0.0.1',
+            'PORT': 3306,
+            'USER': 'root',
+            'PASSWORD': 'root',
+            'NAME': 'django',
+        }
 }
 
 
